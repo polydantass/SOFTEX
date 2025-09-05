f79f719b3e82b76c887a54eb09112f317d3aa7fd
POLYANA DANTAS <polyanadantasps@gmail.com>
2025-09-05 19:36:39 -0300

    Update 05092025exercicio1.py

--- 05092025exercicio1.py
+++ 05092025exercicio1.py
@@ -1,14 +1,27 @@
-def solicitar_nota():
-    while True:
-        try:
-            nota = float(input("Digite uma nota entre 0 e 5: "))
-            if 0 <= nota <= 5:
-                print(f"✅ Nota registrada com sucesso: {nota}")
-                break
-            else:
-                print("❌ Erro: a nota deve estar entre 0 e 5.")
-        except ValueError:
-            print("❌ Erro: digite um número válido.")
+def solicitar_nota(): 
 
-solicitar_nota()
+    while True: 
+
+        try: 
+
+            nota = float(input("Digite uma nota entre 0 e 5: ")) 
+
+            if 0 <= nota <= 5: 
+
+                print(f"✅ Nota registrada com sucesso: {nota}") 
+
+                break 
+
+            else: 
+
+                print("❌ Erro: a nota deve estar entre 0 e 5.") 
+
+        except ValueError: 
+
+            print("❌ Erro: digite um número válido.") 
+
+ 
+ 
+
+solicitar_nota() 
 
