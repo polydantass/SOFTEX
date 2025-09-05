1dc86f2d4c59807a5e2dbaef735b011e6acc1aa0
POLYANA DANTAS <polyanadantasps@gmail.com>
2025-09-05 19:24:35 -0300

    Create 05092025exercicio1.py
    exercício 1 - código feito

---
+++ 05092025exercicio1.py
@@ -0,0 +1,14 @@
+def solicitar_nota():
+    while True:
+        try:
+            nota = float(input("Digite uma nota entre 0 e 5: "))
+            if 0 <= nota <= 5:
+                print(f"✅ Nota registrada com sucesso: {nota}")
+                break
+            else:
+                print("❌ Erro: a nota deve estar entre 0 e 5.")
+        except ValueError:
+            print("❌ Erro: digite um número válido.")
+
+solicitar_nota()
+
